3460b8e22659860b025f47fb2c778dcfd09bf72f
HJ Blom <12938666+hjblom@users.noreply.github.com>
2022-10-24 21:43:48 +0200

    update: Update file mock to os mock to better reflect usage

--- internal/generator/templates/interface_test.go
+++ internal/generator/templates/interface_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/hjblom/fuse/internal/config"
-	file "github.com/hjblom/fuse/internal/util/file/mock"
+	os "github.com/hjblom/fuse/internal/util/osi/mock"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,18 +15,19 @@ func TestInterface(t *testing.T) {
 	defer ctrl.Finish()
 
 	// Given
-	mockFile := file.NewMockInterface(ctrl)
+	mockOS := os.NewMockInterface(ctrl)
+	g := NewInterfaceGenerator(mockOS)
 	c := config.Component{
 		Package: "package",
 		Path:    "internal",
 	}
 
 	// Expect
-	mockFile.EXPECT().Exists(gomock.Any()).Return(false).Times(1)
-	mockFile.EXPECT().WriteFile("internal/package/interface.go", gomock.Any(), gomock.Any()).Return(nil).Times(1)
+	mockOS.EXPECT().Exists(gomock.Any()).Return(false).Times(1)
+	mockOS.EXPECT().WriteFile("internal/package/interface.go", gomock.Any(), gomock.Any()).Return(nil).Times(1)
 
 	// When
-	err := GenerateInterface("test", c, mockFile)
+	err := g.Generate("test", c)
 
 	// Then
 	assert.NoError(t, err)
