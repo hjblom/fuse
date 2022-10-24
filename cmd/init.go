c47fcbaa6e68a2c582a5f893b30e6d22a77163a8
HJ Blom <12938666+hjblom@users.noreply.github.com>
2022-10-24 17:43:23 +0200

    update: Update init command - adding module definition

--- cmd/init.go
+++ cmd/init.go
@@ -4,8 +4,10 @@ Copyright © 2022 HJ Blom
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/hjblom/fuse/internal/commands"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +16,15 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a new fuse project",
 	Run: func(cmd *cobra.Command, args []string) {
-		os.Create(DefaultConfigPath)
+		if len(args) != 1 {
+			fmt.Println("Please provide a module name")
+			os.Exit(1)
+		}
+		err := commands.Init(args[0], PersistentFlagConfigPath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	},
 }
 
