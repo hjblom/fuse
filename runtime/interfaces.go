621a79f5f763e762f6edee9185f47682a381f90c
HJ Blom <12938666+hjblom@users.noreply.github.com>
2023-01-08 22:12:16 +0200

    update: Update runtime

--- runtime/interfaces.go
+++ runtime/interfaces.go
@@ -1,15 +1,6 @@
 package runtime
 
-type Setupper interface {
-	Setup() error
-}
-
 type Service interface {
 	Start() error
 	Stop() error
-}
-
-type Logger interface {
-	Info(log string)
-	Error(log string, err error)
 }
